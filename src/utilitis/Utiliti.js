1bf76ec49d2643de3a15bc965a6138192c86889c
princychodvadiya <princychodvadiya721@gmail.com>
2024-08-22 09:05:53 +0530

    utiliti

--- src/utilitis/Utiliti.js
+++ src/utilitis/Utiliti.js
@@ -3,3 +3,5 @@ export const BATCH_URL='http://localhost:8000/'
 export const SUB_URL='http://localhost:8000/api/v1/sub_categories/'
 
 export const BAKEND_URL='https://fruitable-server-beta.vercel.app/api/v1/'
+
+// export const BAKEND_URL='http://localhost:8000/api/v1/'
