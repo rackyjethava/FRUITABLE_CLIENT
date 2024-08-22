987437ef5b18d743d6cfe8397843c9dbb752ba03
princychodvadiya <princychodvadiya721@gmail.com>
2024-08-22 09:28:44 +0530

    axious

--- src/redux/action/Category.action.js
+++ src/redux/action/Category.action.js
@@ -1,10 +1,11 @@
 
 import axios from 'axios';
 import { ADD_CATEGORY, DELETE_CATEGORY, EDIT_CATEGORY, GET_CATEGORY } from '../ActionType';
+import axiosInstance from '../../utilitis/axiosInstance';
 
 export const getCategory =(data)=> async (dispatch) => {
   try {
-    const response = await axios.get("http://localhost:8000/api/v1/categories/list-categories");
+    const response = await axiosInstance.get("categories/list-categories");
     dispatch({ type: GET_CATEGORY, payload: response.data.data });
   } catch (error) {
     console.error(error);
@@ -13,7 +14,7 @@ export const getCategory =(data)=> async (dispatch) => {
 
 export const addCategory = (data)=> async (dispatch) => {
   try {
-    const response =await axios.post("http://localhost:8000/api/v1/categories/add-category", data,);
+    const response =await axiosInstance.post("categories/add-category", data,);
     dispatch({ type: ADD_CATEGORY, payload: response.data.data });
   } catch (error) {
     console.error(error);
@@ -22,7 +23,7 @@ export const addCategory = (data)=> async (dispatch) => {
 
 export const deleteCategory = (id)=> async (dispatch) => {
   try {
-    await axios.delete(`http://localhost:8000/api/v1/categories/delete-category/${id}`);
+    await axiosInstance.delete(`categories/delete-category/${id}`);
     dispatch({ type: DELETE_CATEGORY, payload: id });
 
   } catch (error) {
@@ -32,7 +33,7 @@ export const deleteCategory = (id)=> async (dispatch) => {
 
 export const updateCategory = (data)=> async (dispatch) => {
   try {
-    await axios.put(`http://localhost:8000/api/v1/categories/update-category/${data._id}`, data);
+    await axiosInstance.put(`categories/update-category/${data._id}`, data);
     dispatch({ type: EDIT_CATEGORY, payload: data });
 
   } catch (error) {
